64d85adac1d484718c6e5583463c8eebea758775
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-31 12:09:32 +0430

    create student exam router

---
+++ server/routes/studentExamRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const studentExamController = require('../controllers/studentExamController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.route('/')
+    .get(authController.protect, studentExamController.getStudentExams)
+    .post(authController.protect, studentExamController.createStudentExam);
+
+router.route('/:id')
+    .get(authController.protect, studentExamController.getStudentExam)
+    .patch(authController.protect, authController.restrictTo('admin'), studentExamController.updateStudentExam)
+    .delete(authController.protect, authController.restrictTo('admin'), studentExamController.deleteStudentExam);
+
+module.exports = router;

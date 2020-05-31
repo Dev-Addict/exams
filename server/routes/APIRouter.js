4e249510a5db8ced6b77ec8c914f185df6b0f964
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-31 11:25:46 +0430

    create exam router

--- server/routes/APIRouter.js
+++ server/routes/APIRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const userRouter = require('../routes/userRouter');
+const examRouter = require('../routes/examRouter');
 
 const router = express.Router({mergeParams: true});
 
 router.use('/users', userRouter);
+router.use('/exams', examRouter);
 
 module.exports = router;

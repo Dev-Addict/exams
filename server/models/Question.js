2eb37a6bc75d665ec14d2ef75bff2d7d0426778b
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-31 11:44:17 +0430

    create question model

---
+++ server/models/Question.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    exam: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Exam'
+    },
+    question: {
+        type: String
+    },
+    questionAsset: {
+        type: String
+    },
+    type: {
+        type: String,
+        enum: {
+            values: ['descriptive', 'test'],
+            message: 'Invalid type'
+        },
+        default: 'test'
+    },
+    option1: {
+        type: String
+    },
+    option1Asset: {
+        type: String
+    },
+    option2: {
+        type: String
+    },
+    option2Asset: {
+        type: String
+    },
+    option3: {
+        type: String
+    },
+    option3Asset: {
+        type: String
+    },
+    option4: {
+        type: String
+    },
+    option4Asset: {
+        type: String
+    },
+    correctOption: {
+        type: String,
+        enum: {
+            values: ['option1', 'option2', 'option3', 'option4'],
+            message: 'Invalid correctOption'
+        }
+    },
+    level: {
+        type: String,
+        required: [true, 'A question must have a level']
+    }
+});
+
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;

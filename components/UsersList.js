b942b9b9b2473e9c2c39b29e68740b15808c353f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-01 18:54:06 +0430

    create delete button and styles

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,24 +1,51 @@
+import {useState} from 'react';
+import Router from "next/router";
+import Link from "next/link";
+import Cookie from 'js-cookie';
+
+import exams from '../api/exams';
+
 const UsersList = ({users, auth}) => {
-    const renderUsers = () => users.filter(user => user.rote === 'student').map(user => (
-        <div className="users-list-user-card" key={user._id}>
-            <table>
-                <tbody>
-                <tr>
-                    <td>Name</td>
-                    <td>{user.name}</td>
-                </tr>
-                <tr>
-                    <td>Username</td>
-                    <td>{user.username}</td>
-                </tr>
-                <tr>
-                    <td>Roles</td>
-                    <td>{user.roles.join(', ')}</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    ));
+
+    const renderUsers = () => users.filter(user => user.rote === 'student').map(user => {
+        const [error, setError] = useState('');
+        return (
+            <div className="users-list-user-card" key={user._id}>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>{user.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td>{user.username}</td>
+                    </tr>
+                    <tr>
+                        <td>Roles</td>
+                        <td>{user.roles.join(', ')}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <Link href={`/edituser/${user._id}`}>
+                    <a className="users-list-user-edit">Edit</a>
+                </Link>
+                <button className="users-list-user-delete" onClick={() => {
+                    exams.delete(`/users/${user._id}`, {
+                        headers: {
+                            authorization: `Bearer ${Cookie.get('jwtClient')}`
+                        }
+                    }).then(res => {
+                        setTimeout(() => Router.push('/dashboard'), 1);
+                    }).catch(err => {
+                        setError(err.response.data.message);
+                    });
+                }}>Delete
+                </button>
+                <div className="error">{error}</div>
+            </div>
+        )
+    });
 
     return (
         <div className="users-list-container">

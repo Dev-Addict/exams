2f55d7fb08c70b4631dd5790bfdf282e0b545418
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-08 20:03:07 +0430

    create student exam page

--- pages/studentexams/[id].js
+++ pages/studentexams/[id].js
@@ -1,15 +1,28 @@
+import ErrorPage from "next/error";
+
+import Question from "../../components/Question";
 import exams from "../../api/exams";
 
-const StudentExam = ({token, studentExam}) => {
+const StudentExam = ({token, studentExam, questions}) => {
+    if (!studentExam)
+        return (
+            <ErrorPage statusCode={404}/>
+        );
+
+    const renderQuestions = () => questions.map((question, index) => (
+        <Question questionNum={index + 1} questionId={questions._id} token={token} {...question}/>
+    ));
+
     return (
         <div>
-            student exam
+            {renderQuestions()}
         </div>
     );
 };
 
 StudentExam.getInitialProps = async ({query: {id}, res: response}, {user}, token) => {
     let studentExam;
+    let questions;
     if (token) {
         if (!/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i.test(id)) {
             if (response)
@@ -27,10 +40,29 @@ StudentExam.getInitialProps = async ({query: {id}, res: response}, {user}, token
         } catch (err) {
             if (response)
                 response.status(404);
-            return {};
+            studentExam = undefined
+        }
+        if (studentExam) {
+            try {
+                const res = await exams.get(`/questions?exam=${studentExam.exam}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+                questions = res.data.data.docs;
+            } catch (err) {
+                if (response)
+                    response.status(404);
+                questions = undefined
+            }
+        }
+        if (questions) {
+            questions = questions.filter(question => studentExam.questions.includes(question._id));
         }
         return {
             studentExam,
+            questions,
             token
         }
     }
